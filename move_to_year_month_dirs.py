f5404d56dd79575bbf38afdfccc4bde13600da23
Dan Born <dan+github@danborn.net>
2019-05-03 13:25:17 -0700

    move_to_year_month_dirs.py: scan source directory recursively for JSON files

--- move_to_year_month_dirs.py
+++ move_to_year_month_dirs.py
@@ -5,12 +5,23 @@ in the JSON file.
 """
 
 import datetime
-import glob
 import json
 import logging
 import os
+import re
 import sys
 import traceback
+
+
+# groups: 1: message ID
+JSON_ID = re.compile(r'(\d+)\.json$', re.IGNORECASE)
+
+
+class Msg(object):
+  def __init__(self, msgId, path, postDate):
+    self.id = msgId
+    self.path = path
+    self.postDate = postDate
 
 
 def move_group(groupName):
@@ -21,20 +32,38 @@ def move_group(groupName):
   logger.propagate = False
   logger.addHandler(h)
   logger.setLevel(logging.INFO)
-  for fname in glob.glob(os.path.join(groupName, "*.json")):
-    with open(fname, "r") as fh:
-      try:
-        # Rename to groupName/year/month/msgid.json if postDate is available.
-        postDate = datetime.datetime.fromtimestamp(
-          int(json.load(fh)['ygData']['postDate']))
+  logger.info('Scanning %s...', groupName)
+  for dirpath, _, filenames in os.walk(groupName, followlinks=True):
+    for fname in filenames:
+      m = JSON_ID.match(fname)
+      if not m:
+        continue
+      path = os.path.join(dirpath, fname)
+      msg = Msg(int(m.group(1)), path, None)
+      with open(path, 'r') as fh:
+        try:
+          msg.postDate = datetime.datetime.fromtimestamp(
+              int(json.load(fh)['ygData']['postDate']))
+        except:
+          logger.error('Failed to extract postDate from %s: %s', path,
+                       traceback.format_exc())
+      if msg.postDate:
         destDir = os.path.join(
-          groupName, postDate.strftime('%Y'), postDate.strftime('%m'))
-        if not os.path.exists(destDir):
-          os.makedirs(destDir)
-        dest = os.path.join(destDir, os.path.basename(fname))
-        os.rename(fname, dest)
-      except:
-        logger.error('%s %s', fname, traceback.format_exc())
+          groupName, msg.postDate.strftime('%Y'), msg.postDate.strftime('%m'))
+      else:
+        destDir = groupName
+      if not os.path.exists(destDir):
+        os.makedirs(destDir)
+      destPath = os.path.join(destDir, '%d.json' % msg.id)
+      if destPath == msg.path:
+        logger.info('Not moving %s', msg.path)
+      elif os.path.exists(destPath):
+        logger.warn('Refusing to clobber file at %s with file at %s',
+                    destPath, msg.path)
+      else:
+        logger.info('Moving %s to %s', msg.path, destPath)
+        os.rename(msg.path, destPath)
+        msg.path = destPath
 
 
 if __name__ == "__main__":
